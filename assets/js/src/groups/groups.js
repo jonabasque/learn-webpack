4bdac9dc28645a678321f1e1049a050ceb1b88c8
jonabasque <jonmultimedia@gmail.com>
2017-04-23 12:00:59 +0200

    Added multi-entry & [name] key to output filename

---
+++ assets/js/src/groups/groups.js
@@ -0,0 +1,1 @@
+alert('groups entry point');
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 var path = require('path');
 
 module.exports = {
-    entry: './assets/js/src/app.js',
+    entry: {
+        index : path.join(__dirname, 'assets', 'js', 'src', 'index', 'index.js'),
+        groups: path.join(__dirname, 'assets', 'js', 'src', 'groups', 'groups.js')
+    },
     output: {
-        path: path.join(__dirname, 'assets/js/dist'),
+        path: path.join(__dirname, 'assets', 'js', 'dist'),
         //publicPath: 'assets',
-        filename: 'app.bundle.js'
+        filename: '[name].bundle.js'
     }
 };
