cc956e7abaacf758ce79ea74f9c7459b9dddc656
jonabasque <jonmultimedia@gmail.com>
2017-04-23 13:00:40 +0200

    Configured babel-loader with .babelrc & preset es2015

--- webpack.config.js
+++ webpack.config.js
@@ -10,5 +10,15 @@ module.exports = {
         path: path.join(__dirname, 'assets', 'js', 'dist'),
         //publicPath: 'assets',
         filename: '[name].bundle.js'
+    },
+    module:{
+        loaders: [
+            {
+                test: /\.js$/, //si la expresión regular devuelve true, por lo que lo va a usar este cargador .
+                loader: 'babel-loader',
+                include: path.join(__dirname, 'assets', 'js', 'src'), //solo pasarán al loader los archivos .js de este directorio
+                //exclude: /node_modules/ //desde la raiz del proyecto excluira lo que indique esta exReg.
+            }
+        ]
     }
 };
