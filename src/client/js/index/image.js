b23129a707c02d7c415072fc980de923c3558c8a
jonabasque <jonmultimedia@gmail.com>
2017-04-27 19:13:34 +0200

    Included no-war img to index header

---
+++ src/client/js/index/image.js
@@ -0,0 +1,3 @@
+let nowar = require('../../img/no-war.png');
+const IMAGE = `<img src="${nowar}">`;
+export default IMAGE;
--- src/client/js/index/index.js
+++ src/client/js/index/index.js
@@ -5,10 +5,15 @@ const headerCSS = require('css/index/header.css');
 const mainCSS = require('css/index/main.css');
 console.log(headerCSS);
 
+import IMAGE from './image';
+
 var titlePartial = require('./title');
 
 var title = document.getElementById('title');
 title.innerHTML = titlePartial;
+
+var header = document.getElementById('header');
+header.innerHTML = IMAGE;
 /*dfdfhghhjggjhsjsfhjhyj
 fgjfjfjfhyjfhjfhj
 fgjfhjfhyjsfhjghsdjkdghkjk
